22660e9847de2e9450ee9989a0036fc12932622f
gpib <rafal.koslowski@gmail.com>
2024-02-24 11:38:04 +0100

    second solution added - sorting function

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import os, art
 
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 # Clearing the Screen
 os.system('cls')
 
 print(art.logo)
+
+def sort_dict(bidding_dictionary):
+    highest_bid = 0
+    winner = ""
+    for bidder in bidding_dictionary:
+        if bidding_dictionary[bidder] > highest_bid:
+            highest_bid = bidding_dictionary[bidder]
+            winner = bidder
+    print(f"The highest bid is {highest_bid} from {bidder}")
 
 next_user = True
 bid_dict ={}
@@ -15,7 +27,8 @@ while next_user == True:
     other_bidder = input("Are there any other bidders? Type 'yes' or 'no'.").lower()
     if other_bidder == 'no':
         next_user = False
-    os.system('cls')
+        sort_dict(bid_dict)
+
 sorted_bid_dict = dict(sorted(bid_dict.items(), key = lambda x:x[1]))
 print(bid_dict)
 print(sorted_bid_dict)
